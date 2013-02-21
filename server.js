e79e6d29c640cd80c2cd6dc10da65cf1e4a866ab
Sarah Chen <swchen@andrew.cmu.edu>
2013-02-21 16:59:48 -0500

    Server and Client v1
    updated server and client.

--- server.js
+++ server.js
@@ -36,31 +36,96 @@ function writeFile(filename, data, callbackFn) {
 
 
 function findRecipe(){
-
+	//hash table? or just iterate through entire array?
 }
 
+//get entire list of recipes
 app.get("/listings", function(request, response){
   response.send({
-    listings: listings,
+    listings: recipeList,
     success: true
   });
 });
 
-app.post();
+//get a page of recipes (?)
 
-app.put();
+//get one recipe
+app.get("/listings/:id", function(request, response){
+  var id = request.params.id;
+  var item = recipeList[id];
+  response.send({
+    listings: item,
+    success: (item !== undefined)
+  });
+});
 
-app.delete();
+//create new recipe
+app.post("/listings", function(request, response) {
+	var item = {"title": request.body.title;
+							"date": new Date();
+							"author": request.body.author;
+							"description": request.body.description;
+							"ingredients": request.body.ingredients;
+							"instructions": request.body.instructions};
+							
+	var successful = 
+      (item.title !== undefined) &&
+      (item.author !== undefined) &&
+			(item.description !== undefined) &&
+			(item.ingredients !== undefined) &&
+			(item.instructions !== undefined);
+	
+	if (successful) {
+    recipeList.push(item);
+    writeFile("data.txt", JSON.stringify(listings));
+  } else {
+    item = undefined;
+  }
+							
+	response.send({ 
+    item: item,
+    success: successful
+  });
+});
 
+//update a recipe
+app.put("/listings/:id", function(request, response){
+  // change listing at index, to the new listing
+  var id = request.params.id;
+  var oldItem = listings[id];
+  var item = {"title": request.body.title;
+							"date": new Date();
+							"author": request.body.author;
+							"description": request.body.description;
+							"ingredients": request.body.ingredients;
+							"instructions": request.body.instructions};
+							
+  item.desc = (item.title !== undefined) ? item.title : oldItem.title;
+  item.author = (item.author !== undefined) ? item.author : oldItem.author;
+  item.description = (item.description !== undefined) ? item.description : oldItem.description;
+  item.ingredients = (item.ingredients !== undefined) ? item.ingredients : oldItem.ingredients;
+	item.instructions = (item.instructions !== undefined) ? item.instructions : oldItem.instructions;
 
+  // commit the update
+  listings[id] = item;
 
+  response.send({
+    item: item,
+    success: true
+  });
+}););
 
-
-
-
-
-
-
+//delete a recipe
+app.delete("/listings/:id", function(request, response){
+  var id = request.params.id;
+  var old = listings[id];
+  listings.splice(id, 1);
+  writeFile("data.txt", JSON.stringify(listings));
+  response.send({
+    listings: old,
+    success: (old !== undefined)
+  });
+});
 
 
 function initServer() {
--- static/client.js
+++ static/client.js
@@ -3,26 +3,81 @@
 //global for all recipe storage
 var recipeList;
 
-
-function refresh(){
-
-
-}
-
-function get(){
-
-}
-
-function add(){
+// addRecipe: Adds a recipe from the text fields in the html to
+// the server recipeList. Resets the text fields.
+function addRecipe(){
 	
 }
 
+// refresh: refreshes the html; redirects to new page? may have to
+// specify the "refresh" function based on what type of command
+// we are executing.
+function refresh(){
+	// Check if the client-side version of the listings exists.
+		if (recipeList === undefined) return;
+
+}
+
+/*
+ * get(): Gets all recipes. Used for browsing recipes.
+*/
+function get(){
+	 $.ajax({
+      type: "get",
+      url: "/listings",
+      success: function(data) {
+        recipeList = data.listings;
+        refresh();
+      }
+    });
+}
+
+/*
+ * getRecipe(): Gets a single recipe. Used for reading, editing, or
+ * deleting a preexisting recipe.
+ * NOT YET IMPLEMENTED
+*/
+
+/*
+ * getRecipePage(): Gets a page of recipes. 
+ * NOT YET IMPLEMENTED
+*/
+
+/*
+ * add(): Adds a recipe to the recipe list.
+*/
+function add(){
+	$.ajax({
+      type: "post",
+      data: {"title": title, "author": author, "description": description, 
+			"ingredients": ingredients, "instructions": instructions},
+      url: "/listings",
+      success: function(data) { 
+				listings.push({
+					title: title;
+					date: new Date();
+					author: author;
+					description: description;
+					ingredients: ingredients;
+					instructions: instructions;
+				});
+				refreshDOM();
+			}
+    });
+}
+
+/*
+ * edit(id): Edits a preexisting recipe with the id "id".
+*/
 function edit(id){
 	
 }
 
+/*
+ * del(id): Deletes a preexisting recipe with the id "id".
+*/
 function del(id){
-
+	
 }
 
 $(document).ready(function() {
