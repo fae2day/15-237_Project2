93cf05741d5354a4ca6c30ffdab349d2b7374c28
Angela Qiu <aqiu@andrew.cmu.edu>
2013-02-25 05:30:43 -0500

    toggling works now :)
    changed some names of ids and classes... there are too many x_x;;; tried
    to make it prettier, may have failed. bleh.

---
+++ static/buttonfn.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+	hideEverything();
+	$('#welcomePage').show();
+	console.log("loaded!");
+
+});
+
+$('#menuview').click(function(){
+	
+	hideEverything();
+	$('#displayRecipe').show();
+});
+
+$('#menunew').click(function(){
+	
+	hideEverything();
+	$('#newRecipe').show();
+});
+
+$('#menusearch').click(function(){
+	
+	hideEverything();
+	$('#searchRecipe').show();
+});
+
+$('#menuedit').click(function(){
+	
+	hideEverything();
+	$('#editRecipe').show();
+});
+
+function hideEverything(){
+	
+	$('#newRecipe').hide();
+	$('#searchRecipe').hide();
+	$('#editRecipe').hide();
+	$('#deleteRecipe').hide();
+	$('#displayRecipe').hide();
+	$('#welcomePage').hide();
+}
